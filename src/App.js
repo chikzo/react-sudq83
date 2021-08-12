23e92ee6404a83d906345a5bd0786b539bb9714d
chikzo <chegosam97@gmail.com>
2021-08-12 03:29:27 -0700

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,96 +1,104 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
-const App = () =>{
-  
-  const [item, setItem ] = React.useState("")  
-  const [itemList, setItemList] = React.useState([])
-  const [quan , setQuan]= React.useState("")
-  const [quanList, setQuanList] = React.useState([])
-  const [desc , setDesc]= React.useState("")
-  const [descList, setItemDesc] = React.useState([])
+const App = () => {
+  const [item, setItem] = React.useState({
+    itemName: '',
+    description: '',
+    quantity: ''
+  });
+  const [itemList, setItemList] = React.useState([]);
+  const [formKey, setFormkey] = React.useState([10])
 
+  function onSubmitHandler(e) {
+    e.preventDefault();
+    console.log();
 
-  function onSubmitHandler(e){
-    e.preventDefault()
-    console.log(quan);
-  
-  const addItemDetails ={
-    id : new Date().getTime(),
-    itemName: item,
-    descDet: desc,
-    quanName: quan,
-    complete: false
-  }
- 
-    setItemList([...itemList].concat(addItemDetails))
-    setItem("")
+    const addItemDetails = {
+      id: new Date().getTime(),
+      itemName: item.itemName,
+      description: item.description,
+      quantity: item.quantity,
+      complete: false
+    };
+
+    setItemList([...itemList].concat(addItemDetails));
+    setItem('');
     console.log(itemList);
-    
+    setFormkey(formKey+1);
   }
 
-  function deleteItem(id){
-
-    let updatedList = [...itemList].filter((item) => item.id !== id)
-    setItemList(updatedList)
-  
+  function deleteItem(id) {
+    let updatedList = [...itemList].filter(item => item.id !== id);
+    setItemList(updatedList);
   }
-
 
   return (
     <div>
-     
-      <form onSubmit ={onSubmitHandler}>
-      <fieldset>
-
-        <legend><h1>Add Items</h1> </legend>
-        <label for="name">Name</label> <br/>
-        <input class="itemInput" type="text" value ={item}
-          onChange = {(e) => setItem(e.target.value)}
-          required placeholder="Name"/>
-          <br/>
-
-        <label for="description">Description </label>
-        <textarea rows = "5" cols = "50" name = "description" placeholder = "Enter description..." class="form-control" onChange= {(e) => setDesc(e.target.value)}></textarea>
-
-        <label for="quantity">Quantity</label><br/>
-
-        <input for="quan" value={quan}
-          onChange = {(e) => setQuan(e.target.value)}
-          required placeholder = '0' />
-
-      
-      <button>Add</button>
-      </fieldset>
+      <form onSubmit={onSubmitHandler} key={formKey}>
+        <fieldset>
+          <legend>
+            <h1>Add Items</h1>{' '}
+          </legend>
+          <label for="name">Name</label> <br />
+          <input
+            class="itemInput"
+            type="text"
+            value={item.itemName}
+            onChange={e => setItem({ ...item, itemName: e.target.value })}
+            required
+            placeholder="Name"
+          />
+          <br />
+          <label for="description">Description </label>
+          <textarea
+            rows="5"
+            cols="50"
+            name="description"
+            placeholder="Enter description..."
+            class="form-control"
+            onChange={e => setItem({ ...item, description: e.target.value })}
+          />
+          <label for="quantity">Quantity</label>
+          <br />
+          <input
+            for="quantity"
+            value={item.quantity}
+            type="number"
+            onChange={e => setItem({ ...item, quantity: e.target.value })}
+            required
+            placeholder="0"
+          />
+          <button>Add</button>
+        </fieldset>
       </form>
 
-      <br/>
-
-    {itemList.map((data) =>(
-    <div key={data.id} class="list-style">
-      
-        <div className="button-style">
+      <div>
         <fieldset>
-          <legend><h1>Shopping List</h1></legend>
-        {data.itemName}
-        <br/>
-        {data.descDet}
-        <br/>
-        {data.quanName}
-        <br/>
+          <legend>
+            <h1>Shopping List</h1>
+          </legend>
 
-        
-        <div className="item-style">
-          
-          </div>
-        <button onClick={() => deleteItem(data.id)}>Delete</button>
+          {itemList.map(data => (
+            <div key={data.id} class="list-style">
+              <div className="button-style">
+                {data.itemName}
+                <br />
+                {data.description}
+                <br />
+                {data.quantity}
+                <br />
+
+                <button onClick={() => deleteItem(data.id)}>Delete</button>
+
+                <button class="cancel" onClick= {this.onChange}>Cancel</button>
+              </div>
+            </div>
+          ))}
         </fieldset>
-        <button class ="cancel">Cancel</button>
-        </div>
-        
-</div> ))}
-   
+      </div>
+      <br />
     </div>
   );
-}
+};
 export default App;
